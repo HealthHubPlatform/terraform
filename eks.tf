373a96a29f729b3177b3b822b13c546b57908161
Kuldeep Chowhan <kchowhan@sofi.org>
2020-08-26 22:58:57 -0500

    add nlb ingress controller

--- eks.tf
+++ eks.tf
@@ -10,14 +10,6 @@ provider "external" {
   version = "~> 1.2"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
-}
-
 data "terraform_remote_state" "global" {
   backend = "s3"
   config = {
